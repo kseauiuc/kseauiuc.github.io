586967c00de7d7af50dde745e8349292893bc347
Henry Seong Wook Ha <sha16@illinois.edu>
2017-02-18 22:56:53 -0600

    clearing fix

--- js/postdata.js
+++ js/postdata.js
@@ -38,7 +38,6 @@ $(_id).submit(function(event){
         // Log a message to the console
         console.log("Hooray, it worked!");
         for(child in _children){
-          console.log(child);
           $(_children[child]).val('');
         }
     });
