f33cb016f6068fcb8860eff84e27e513f8192669
Henry Seongwook Ha <sha16@illinois.edu>
2017-11-04 18:36:03 -0500

    change to http for youtube

--- js/youtube_parsing.js
+++ js/youtube_parsing.js
@@ -25,7 +25,7 @@ $.ajax('https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResu
             // 2. This code loads the IFrame Player API code asynchronously.
             var tag = document.createElement('script');
         
-            tag.src = "https://www.youtube.com/iframe_api";
+            tag.src = "http://www.youtube.com/iframe_api";
             var firstScriptTag = document.getElementsByTagName('script')[0];
             firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
         }
